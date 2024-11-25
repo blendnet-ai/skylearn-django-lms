ac1b60a6bcaefe54b2ccb250506d4ef5aa6308d2
Abhip32 <abhishek.a.patil112@gmail.com>
2024-11-25 20:31:03 +0530

    fixed import

--- evaluation/views.py
+++ evaluation/views.py
@@ -8,7 +8,6 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django.utils.dateformat import DateFormat
 from rest_framework import status
-from rest_framework.permissions import IsLoggedIn
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.http import HttpResponseBadRequest
