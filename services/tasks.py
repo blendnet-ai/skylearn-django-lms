6008ac703f2b6cf21c78e90845bb4e662de901ad
Abhip32 <abhishek.a.patil112@gmail.com>
2024-11-26 15:50:06 +0530

    commented resume reakted celery task for now

--- services/tasks.py
+++ services/tasks.py
@@ -1,9 +1,9 @@
-from celery import shared_task
-from .resume_builder_service import ResumeBuilderService
+# from celery import shared_task
+# from .resume_builder_service import ResumeBuilderService
 
 
-@shared_task(queue='services_queue')
-def build_resume_task(user_id ,resume_id):
-    ResumeBuilderService().build_resume(user_id,resume_id)
+# @shared_task(queue='services_queue')
+# def build_resume_task(user_id ,resume_id):
+#     ResumeBuilderService().build_resume(user_id,resume_id)
     
     
