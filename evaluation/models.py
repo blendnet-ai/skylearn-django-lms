6b0de79674cb6466897b034fe48eff6ff2d0dca7
Abhip32 <abhishek.a.patil112@gmail.com>
2024-12-18 12:28:44 +0530

    start date due date end date in assessment config and locking assessment based on this

--- evaluation/models.py
+++ evaluation/models.py
@@ -386,6 +386,9 @@ class AssessmentGenerationConfig(models.Model):
     number_of_attempts = models.IntegerField(default=2)
     test_duration = models.DurationField()
     display_data = models.JSONField(default=dict)
+    start_date = models.DateTimeField(blank=True, null=True)
+    end_date=models.DateTimeField(blank=True,null=True)
+    due_date=models.DateTimeField(blank=True,null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
