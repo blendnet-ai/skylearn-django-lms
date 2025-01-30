f4a3a0d76a7003d44585b7d8078a729c9ac53c26
Abhishek Patil <91268212+Abhip32@users.noreply.github.com>
2025-01-30 09:07:56 +0530

    removed date as global variable (#29)

--- reports/management/generate_report_sheet/report_sheet_generator.py
+++ reports/management/generate_report_sheet/report_sheet_generator.py
@@ -15,9 +15,9 @@ import json
 from datetime import datetime
 
 User = get_user_model()
-date=datetime.now().date()
 # Optimized function to get all required data at once
 def fetch_all_required_data():
+    date=datetime.now().date()
     # Fetch all students
     students = StudentRepository.get_all_students()
 
