de164ed827cd2109835a7d63dc1e6b37c341c2e2
Abhip32 <abhishek.a.patil112@gmail.com>
2025-02-04 12:52:13 +0530

    API To change student status for admin role

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 
 from .views import (
     enroll_students_in_batch,
-    get_course_provider
+    get_course_provider,
+    update_student_status
 )
 
 
@@ -13,5 +14,10 @@ urlpatterns = [
         enroll_students_in_batch,
         name="enroll_students_in_batch",
     ),
-    path("get-course-provider",get_course_provider,name="get_course_provider")
+    path("get-course-provider",get_course_provider,name="get_course_provider"),
+    path(
+        'student/<int:student_id>/status/',
+        update_student_status,
+        name='update_student_status'
+    ),
 ]
