cf8b5610b3a24f409e58702f0b948d3d48034301
Abhishek Patil <91268212+Abhip32@users.noreply.github.com>
2025-02-10 17:44:41 +0530

    Abhishek/course hours feature (#36)
    * concent form + course hours
    
    * chanege in format of response
    
    * seperate object for form
    
    * adding card type in response
    
    * sending time in minutes

--- course/models.py
+++ course/models.py
@@ -29,6 +29,7 @@ class Course(models.Model):
     summary = models.TextField(max_length=200, blank=True)
     course_provider= models.ForeignKey( 'accounts.courseprovider', on_delete=models.CASCADE)
     drive_folder_link = models.CharField(max_length=255, blank=True)
+    course_hours=models.IntegerField(default=0)
 
 
     def __str__(self):
