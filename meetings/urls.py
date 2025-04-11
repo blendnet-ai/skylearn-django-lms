030f8effa3bfb7177bec7ea375fa1fabc33190e8
Abhip32 <abhishek.a.patil112@gmail.com>
2025-04-11 17:19:41 +0530

    adding recording delete and api to add additional recordings to meetings

--- meetings/urls.py
+++ meetings/urls.py
@@ -1,10 +1,36 @@
 from django.urls import path
-from .views import MarkAttendanceAndRedirect,GetJoiningUrl,GetCommonJoiningUrl,MarkAttendanceCommonURLAndRedirect
+from .views import (
+    MarkAttendanceAndRedirect,
+    GetJoiningUrl,
+    GetCommonJoiningUrl,
+    MarkAttendanceCommonURLAndRedirect,
+    UploadAdditionalRecording,
+    DeleteRecording,
+)
 
 urlpatterns = [
-    path('join/<uuid:attendance_id>/', MarkAttendanceAndRedirect.as_view(), name='mark_attendance'),
-    path('get-joining-url/<int:meeting_id>/',GetJoiningUrl.as_view(), name='get_joining_url'),
-    path('join-meeting/<str:reference_id>/', MarkAttendanceCommonURLAndRedirect.as_view(), name='join_meeting'), #common url marking attendance
-    path('get-meeting-url/', GetCommonJoiningUrl.as_view(), name='get_meeting_url'),#common url get meetingn url
-
+    path(
+        "join/<uuid:attendance_id>/",
+        MarkAttendanceAndRedirect.as_view(),
+        name="mark_attendance",
+    ),
+    path(
+        "get-joining-url/<int:meeting_id>/",
+        GetJoiningUrl.as_view(),
+        name="get_joining_url",
+    ),
+    path(
+        "join-meeting/<str:reference_id>/",
+        MarkAttendanceCommonURLAndRedirect.as_view(),
+        name="join_meeting",
+    ),  # common url marking attendance
+    path(
+        "get-meeting-url/", GetCommonJoiningUrl.as_view(), name="get_meeting_url"
+    ),  # common url get meetingn url
+    path(
+        "upload-additional-recording/<int:meeting_id>/",
+        UploadAdditionalRecording.as_view(),
+        name="upload-additional-recording",
+    ),
+    path("delete-recording/", DeleteRecording.as_view(), name="delete-recording"),
 ]
